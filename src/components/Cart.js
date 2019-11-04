5ac4f9b67340aa156d08259919531ecbe71e55ad
kimalex415 <alexkiminbox@gmail.com>
2019-11-04 12:04:17 -0800

    context and details page

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Cart extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>hello from Cart</h3>
+            </div>
+        )
+    }
+}
